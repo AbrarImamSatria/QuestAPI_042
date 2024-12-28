9d4ebc0ac8f82bf1f25aa4267e61e38ddbd33f73
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-29 02:39:44 +0700

    memindahkan file ke package ui

--- app/src/main/java/com/example/pertemuan12/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/PenyediaViewModel.kt
@@ -1,11 +1,12 @@
-package com.example.pertemuan12.ui.viewmodel
+package com.example.pertemuan12.ui
 
-import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan12.MahasiswaApplications
+import com.example.pertemuan12.ui.viewmodel.HomeViewModel
+import com.example.pertemuan12.ui.viewmodel.InsertViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
