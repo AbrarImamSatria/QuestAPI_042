bdf148acedfddba9e2f8e63ea7d278287eaff70b
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-29 00:12:53 +0700

    membuat class InsertViewModel dan fun updateInsertMhsState

--- app/src/main/java/com/example/pertemuan12/ui/theme/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/viewmodel/InsertViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.pertemuan12.ui.theme.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan12.model.Mahasiswa
-import kotlinx.serialization.SerialName
+import com.example.pertemuan12.repository.MahasiswaRepository
+
+class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+}
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
