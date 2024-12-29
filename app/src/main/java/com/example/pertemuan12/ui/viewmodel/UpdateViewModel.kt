409b1bd9bfaf4b4d169797243936d957789d7f2a
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-29 15:14:11 +0700

    membuat fun UpdateUiEveny, UpdateUiState dan UpdateUiState

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/UpdateViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.pertemuan12.ui.viewmodel
 
+import com.example.pertemuan12.model.Mahasiswa
+
 data class UpdateUiState(
     val updateUiEvent: UpdateUiEvent = UpdateUiEvent()
 )
@@ -11,4 +13,26 @@ data class UpdateUiEvent(
     val jenisKelamin: String = "",
     val kelas: String = "",
     val angkatan: String = ""
+)
+
+fun UpdateUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUpdateUiState(): UpdateUiState = UpdateUiState(
+    updateUiEvent = toUpdateUiEvent()
+)
+
+fun Mahasiswa.toUpdateUiEvent(): UpdateUiEvent = UpdateUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
 )
