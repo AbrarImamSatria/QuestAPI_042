8574c0e6ed34a1e68ca5598d9f10748aba16eadc
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-29 14:32:49 +0700

    membuat private fun OnLoadingDetail

--- app/src/main/java/com/example/pertemuan12/ui/view/DetailView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/DetailView.kt
@@ -1,17 +1,31 @@
 package com.example.pertemuan12.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.pertemuan12.R
 import com.example.pertemuan12.model.Mahasiswa
 
+
+@Composable
+private fun OnLoadingDetail(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
 
 @Composable
 fun DetailLayout(
