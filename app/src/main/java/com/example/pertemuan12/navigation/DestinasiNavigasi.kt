731ad8948de8d29af65afc3a6e685a8904937520
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-29 15:19:59 +0700

    menambahkan object DestinasiHome, object DestinasiEntry, object DestinasiDetail dan object DestinasiEdit

--- app/src/main/java/com/example/pertemuan12/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pertemuan12/navigation/DestinasiNavigasi.kt
@@ -4,3 +4,24 @@ interface DestinasiNavigasi {
     val route: String
     val titleRes: String
 }
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home"
+}
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mahasiswa"
+}
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail/{nim}"
+    override val titleRes = "Detail Mahasiswa"
+    const val nimArg = "nim"
+}
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit/{nim}"
+    override val titleRes = "Edit Mahasiswa"
+}
