b50358d69538e109cdd82dec26998da5b773e066
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-29 15:17:34 +0700

    membuat fun EditBody

--- app/src/main/java/com/example/pertemuan12/ui/view/UpdateView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/UpdateView.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +16,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.pertemuan12.ui.viewmodel.UpdateUiEvent
+import com.example.pertemuan12.ui.viewmodel.UpdateUiState
+
+@Composable
+fun EditBody(
+    updateUiState: UpdateUiState,
+    onMhsValueChange: (UpdateUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputEdit(
+            updateUiEvent = updateUiState.updateUiEvent,
+            onValueChange = onMhsValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan Perubahan")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
