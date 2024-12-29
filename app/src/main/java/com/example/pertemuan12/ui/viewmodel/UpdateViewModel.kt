b809eae819216501c7700c1befffa87b3e9b9d9c
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-29 15:15:07 +0700

    membuat class UpdateViewModel, fun updateMhs dan getMhsByNim

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/UpdateViewModel.kt
@@ -1,6 +1,43 @@
 package com.example.pertemuan12.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class UpdateViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(UpdateUiState())
+        private set
+
+    fun updateUiState(updateUiEvent: UpdateUiEvent) {
+        uiState = UpdateUiState(updateUiEvent = updateUiEvent)
+    }
+
+    suspend fun updateMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.updateMahasiswa(nim, uiState.updateUiEvent.toMhs())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    suspend fun getMhsByNim(nim: String) {
+        viewModelScope.launch {
+            try {
+                val result = mhs.getMahasiswabyNim(nim)
+                uiState = result.toUpdateUiState()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class UpdateUiState(
     val updateUiEvent: UpdateUiEvent = UpdateUiEvent()
